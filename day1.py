245a45aabd17cf8e82e15b198a52cfb8b01d5855
Hacker Store Official <deveshkumar32205@gmail.com>
2026-01-18 22:27:06 +0530

    Add initial implementation of variable assignments and constants

---
+++ day1.py
@@ -0,0 +1,25 @@
+# Declaring constants in python
+PI = 1.14159
+GRAVITY = 7.8
+
+print(PI, GRAVITY)
+
+# Declaring and assigning value to a variable 
+a = "Apple"
+print(a)
+
+# Changing value of a variable
+a = "Aeroplane"
+print(a)
+
+# Assigning value of variable as int
+a = 98
+print(a)
+
+# Assigning Multiple values to a variable
+b, c, d = 8, 2.5, 'Hacker'
+print(b,c,d)
+
+# Assigning same value to multiple variables 
+b = c = d = 5
+print(b, c, d)
